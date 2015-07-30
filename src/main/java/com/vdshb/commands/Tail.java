1150ecd374fbf9877afcb314d4e8e9b2ab9cc330
vadim-shb <vadim.shabanov@gmail.com>
2015-07-31 01:46:49 +0300

    Add tail command

--- src/main/java/com/vdshb/commands/Tail.java
+++ src/main/java/com/vdshb/commands/Tail.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.System.out;
 
@@ -28,13 +30,80 @@ public class Tail {
     }
 
     private static void tail(Path fileToRead, int tailLinesNumber) {
-
+        StringVault tail = new StringVault(tailLinesNumber);
         try (BufferedReader reader = Files.newBufferedReader(fileToRead, StandardCharsets.UTF_8)) {
             String line = null;
             while ((line = reader.readLine()) != null)
-                out.println(line);
+                tail.addString(line);
+            for (String tailLine : tail.getStrings())
+                out.println(tailLine);
         } catch (IOException e) {
             out.println("Can not read file");
+        } catch (IllegalArgumentException ex) {
+            out.println("Illegal lines number");
         }
     }
+
+    private static class StringVault {
+        private final String[] strings;
+        private int pointer = -1;
+        private int capacity = 0;
+        private boolean round = false;
+
+        public StringVault(int vaultCapacity) {
+            if (vaultCapacity < 1) throw new IllegalArgumentException();
+            this.strings = new String[vaultCapacity];
+            this.capacity = vaultCapacity;
+        }
+
+        public void addString(String str) {
+            incrementPointer();
+            strings[pointer] = str;
+        }
+
+        public List<String> getStrings() {
+            List<String> retval = new ArrayList<>(capacity);
+            if (round) {
+                for (int i = 0; i < capacity; i++) {
+                    incrementPointer();
+                    retval.add(strings[pointer]);
+                }
+
+            } else {
+                for (int i = 0; i <= pointer; i++) {
+                    retval.add(strings[i]);
+                }
+            }
+            return retval;
+        }
+
+        private void incrementPointer() {
+            pointer++;
+            if (pointer == capacity) {
+                pointer = 0;
+                round = true;
+            }
+        }
+
+    }
+//
+//    private static void tail(Path fileToRead, int tailLinesNumber) {
+//        ByteBuffer buffer = ByteBuffer.allocate(200);
+//        CharBuffer charBuffer;
+//        Charset charSet = StandardCharsets.UTF_8;
+//        try(FileChannel fc = FileChannel.open(fileToRead, StandardOpenOption.READ)){
+//            long position=0;
+//            fc.position(position);
+//            fc.read(buffer);
+//            buffer.flip();
+//            charBuffer = charSet.decode(buffer);
+//            out.println(charBuffer.toString());
+//
+//        } catch (IOException e) {
+//            out.println("Can not read file");
+//        }
+//
+//
+//
+//    }
 }

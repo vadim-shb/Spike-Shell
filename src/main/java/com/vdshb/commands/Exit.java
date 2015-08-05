329f0e4ba5e10265fe02ff500d9d440a956d874a
vadim-shb <vadim.shabanov@gmail.com>
2015-08-06 01:11:18 +0300

    Improve exit

--- src/main/java/com/vdshb/commands/Exit.java
+++ src/main/java/com/vdshb/commands/Exit.java
@@ -1,8 +1,23 @@
 package com.vdshb.commands;
 
+import static java.lang.System.out;
+
 public class Exit {
 
     public static void exit(String[] args) {
+        if (args.length > 1) {
+            out.println("Incorrect command parameters.");
+            return;
+        }
+        if (args.length == 1) {
+            try {
+                int exitCode = Integer.valueOf(args[0]);
+                System.exit(exitCode);
+            } catch (NumberFormatException e) {
+                out.println("Incorrect exit code.");
+                return;
+            }
+        }
         System.exit(0);
     }
 }

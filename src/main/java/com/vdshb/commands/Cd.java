4e2c47a30fb38d5ef0a7166baae823d0beb89758
vadim-shb <vadim.shabanov@gmail.com>
2015-08-05 09:57:50 +0300

    Improve cd

--- src/main/java/com/vdshb/commands/Cd.java
+++ src/main/java/com/vdshb/commands/Cd.java
@@ -3,6 +3,7 @@ package com.vdshb.commands;
 import com.vdshb.CurrentPath;
 import com.vdshb.exceptions.PathNotFound;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static java.lang.System.out;
@@ -12,6 +13,10 @@ public class Cd {
         String where = args[args.length - 1];
         try {
             Path destination = CurrentPath.getExistRelatedPath(where);
+            if (!Files.isDirectory(destination)){
+                out.println("Path is not a directory");
+                return;
+            }
             CurrentPath.setCurrentPath(destination.normalize());
         } catch (PathNotFound pathNotFound) {
             out.println("Path not exists");
